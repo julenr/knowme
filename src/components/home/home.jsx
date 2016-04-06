dfa8eba115906eb6b28d66650c61a4292be40bf1
Julen <isusk246@gmail.com>
2016-04-06 21:13:08 +1200

    added Roboto font

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -27,22 +27,22 @@ class Home extends React.Component {
   render() {
     return (
       <section id="home" className='section main-section active' >
-        <div className="row" >
+        <div className="flex-container" >
 
-          <div className="col-lg-6 col-md-5 col-xs-12 img-col-main">
+          <div className="flex-avatar">
             <div className="person-img" >
               <img src={require('../../assets/images/avatar.svg')} alt="" />
             </div>
           </div>
 
-          <div className="col-lg-6 col-md-7 col-xs-12 text-col-main">
+          <div className="text-col-main">
             <div className="intro-text-col">
               <div className="inner">
                 <div className="intro-text">
                   <h1>I'm Julen Rojo</h1>
-                  <span>A friend, buddy and dude from earth</span>
+                  <span>a JavaScript functional programmer</span>
                   <p>
-                    Euismod consulatu eos eu. Vocent facilis sensibus vel ut, quo an nihil qualisque. Mea eu nulla prompta. Dicat velit at usum, malis euismod vim an
+                    "Sometimes, the elegant implementation is just a function. Not a method. Not a class. Not a framework. Just a function."
                   </p>
                   <div className="intro-btns" >
                     <a href="#" className="btn-material" >
