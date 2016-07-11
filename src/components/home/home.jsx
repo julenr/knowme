15fe7eff56703c5682803ca2deae42cdd3040d63
julenr <isusk246@gmail.com>
2016-07-11 19:06:09 +1200

    Improved Home, and BEM styled

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -28,39 +28,34 @@ class Home extends React.Component {
   render() {
     const {introTitle, introSubTitle, cites, hireMe, downloadResume} = this.props.Home;
     return (
-      <section id="home" className='section main-section active' >
-        <div className="flex-container" >
-          <div className="flex-avatar">
-            <div className="avatar" />
+      <section className='section main-section' >
+        <div className="home__flex-container" >
+          <div className="home__flex-avatar">
+            <div className="home__flex-avatar__avatar" />
           </div>
-          <div className="text-col-main">
-            <div className="intro-text-col">
-              <div className="inner">
-                <div className="intro-text">
-                  <h1>{introTitle}</h1>
-                  <h2>{introSubTitle}</h2>
-                  <p>{cites[0]}</p>
+          <div className="home__text-col-main">
+            <div className="intro-text">
+              <h1>{introTitle}</h1>
+              <h2>{introSubTitle}</h2>
+              <p>{cites[0]}</p>
+            </div>
+            <div className="intro-btns" >
+              <a href={require('../../assets/rojojulenresume.pdf')} download>
+                <div className="svg-wrapper">
+                  <svg height="60" width="240">
+                    <rect className="shape" height="60" width="240" />
+                  </svg>
+                  <div className="text">{downloadResume}</div>
                 </div>
-                <div className="intro-btns" >
-                  <a href={require('../../assets/rojojulenresume.pdf')} download>
-                    <div className="svg-wrapper">
-                      <svg height="60" width="240">
-                        <rect className="shape" height="60" width="240" />
-                      </svg>
-                      <div className="text">{downloadResume}</div>
-                    </div>
-                  </a>
-                  <Link to="/contact" onClick={() => props.menuButtonClick()}>
-                    <div className="svg-wrapper">
-                      <svg height="60" width="240">
-                        <rect className="shape" height="60" width="240" />
-                      </svg>
-                      <div className="text">{hireMe}</div>
-                    </div>
-                  </Link>
+              </a>
+              <Link to="/contact" onClick={() => props.menuButtonClick()}>
+                <div className="svg-wrapper">
+                  <svg height="60" width="240">
+                    <rect className="shape" height="60" width="240" />
+                  </svg>
+                  <div className="text">{hireMe}</div>
                 </div>
-                <br /><br />
-              </div>
+              </Link>
             </div>
           </div>
         </div>
